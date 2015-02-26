e02a45749be2e77d8e1def0a17a5fb78722d5f62
Stefano Charissis <stefano.charissis@gmail.com>
2015-02-26 17:45:51 +1100

    Update: reformer; test - get Packages() and getDependencies() helper functions.

--- reformer/src/test/scala/ReformerSuite.scala
+++ reformer/src/test/scala/ReformerSuite.scala
@@ -6,8 +6,8 @@ import org.apache.spark.SparkConf
 import org.scalatest.BeforeAndAfter
 import org.scalatest.FunSuite
 
-import com.liberator._
 import com.liberator.LocalSparkContext._
+import com.liberator.PackageJson._
 
 class ReformerSuite extends FunSuite with LocalSparkContext {
 
@@ -19,20 +19,44 @@ class ReformerSuite extends FunSuite with LocalSparkContext {
     sc
   }
 
+  private def getPackages(sc: SparkContext, source: String, regex: String)
+    : org.apache.spark.rdd.RDD[PackageJson] = {
+    return Reformer.run(sc, source, regex).flatMap(identity)
+  }
+
+  private def getDependencies(packages : org.apache.spark.rdd.RDD[PackageJson])
+    : Map[String,List[Dependency]] = {
+    return packages
+      .map( p => (p.name, p.dependencies)).groupByKey
+      .map( pair => (pair._1, pair._2.flatMap(identity).toList))
+      .collect
+      .toMap
+  }
+
   val test_source = "src/test/resources/"
 
-  test("instantiates a SparkContext") {
+  test("SparkContext - starts/stops") {
     withSpark(newSparkContext()) { sc =>
       assert(sc != null)
     }
     assert(sc == null)
   }
 
-  test("reforms") {
+  test("simple - runs okay") {
     withSpark(newSparkContext()) { sc =>
-      assert(Reformer.output == null)
-      Reformer.run(sc, source = "../crawler/output/repos/raw/github", file_regex = "/f*/*/package_*.json")
-      assert(Reformer.output.count != 0)
+      val output = Reformer.run(sc, source = test_source, file_regex = "simple/identical/package*.json")
+      assert(output.count != 0)
+    }
+  }
+
+  test("simple - identical inputs") {
+    withSpark(newSparkContext()) { sc =>
+      val packages = getPackages(sc, test_source, "simple/identical/package*.json")
+      val dependencies = getDependencies(packages)
+
+      assert(packages.count === 2)  // 2 input package.json's
+      assert(dependencies.size === 1) // 1 resultant Package (they were identical)
+      assert(dependencies("d3").size === 4) // 4 discrete Dependencies
     }
   }
 }
