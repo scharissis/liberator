1e71ead25c3eb881a1e35a66667485afa3236820
Stefano Charissis <stefano.charissis@gmail.com>
2015-02-27 08:33:59 +1100

    Fix: util: encode_b64 JSON-ifies data before encoding.

--- test/util/encode_b64.py
+++ test/util/encode_b64.py
@@ -4,10 +4,13 @@ import base64
 import json
 import sys
 
+def pretty(d):
+	return json.dumps(d, indent=2)
+
 filename = sys.argv[1]
 
 data = None
 with open(filename, "r") as f:
 	data = json.load(f)
-encoded = base64.b64encode(str(data))
+encoded = base64.b64encode(pretty(data))
 print(encoded)
