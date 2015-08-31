c1c0bf4083b8c2503384aba6f9079393a77e4aab
Stefano Charissis <stefano.charissis@gmail.com>
2015-08-31 16:10:37 +1000

    Update: ingestor; Neo4J debugging script updated for 2.2.5.

--- ingestor/test/debug/neo4j.sh
+++ ingestor/test/debug/neo4j.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Check Neo4J is installed.
 hash neo4j 2>/dev/null || \
 	{ echo >&2 "Neo4J not found; is it installed?"; exit 1; }
@@ -10,13 +12,13 @@ cd test/debug
 
 neo_dir="$(dirname $(which neo4j))/.."
 db_dir="${neo_dir}/data/graph.db"
-nodes="packages_header.csv"
+nodes="$PWD/packages_header.csv"
 for f in output/packages.csv/part-*; do
-	nodes="${nodes} $f"
+	nodes="${nodes},$PWD/$f"
 done
-deps="dependencies_header.csv"
+deps="$PWD/dependencies_header.csv"
 for f in output/dependencies.csv/part-*; do
-	deps="${deps} ${f}"
+	deps="${deps},$PWD/${f}"
 done
 
 echo "DB           : ${neo_dir}/data/graph.db"
@@ -30,5 +32,5 @@ neo4j-import --into "${db_dir}" \
 	--id-type STRING \
 	--nodes "${nodes}" \
 	--relationships "${deps}" --stacktrace
-	
+
 neo4j console
